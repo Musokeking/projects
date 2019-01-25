2502e69475002ec213fd9a503296f1770f074d63
Musokeking <musokekng@gmail.com>
2019-01-25 17:28:05 +0300

    add function

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+x=0
+dict={}
+for x in range(x,15):
+    sqrt= x**2
+    dict[x]=sqrt
+
+print(dict)
--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,10 @@
-age = int(input("What is your age: "))
-if age <= 17:
-    print('You are a minor')
-elif age >= 18 and age <= 36:
-    print ('You are a youth')
-else:
-    print ('You are an elder')
+value = int(input("What is your age: "))
+def age(age):
+    if age <= 17:
+        print('You are a minor')
+    elif age >= 18 and age <= 36:
+        print ('You are a youth')
+    else:
+        print ('You are an elder')
+
+age(value)
